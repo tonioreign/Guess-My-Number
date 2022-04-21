0b1101898ddbf8aac48f2f8a466d1fc1635ca6e7
tonioreign <tonioreign@gmail.com>
2022-04-21 13:11:35 -0700

    Added guess function with applied DOM

--- js/app.js
+++ js/app.js
@@ -2,12 +2,42 @@
 
 const againBtn = document.querySelector('.again');
 const randomNumber = document.querySelector('.number');
-const guess = document.querySelector('.guess');
+const checkInput = document.querySelector('.input');
 const check = document.querySelector('.check');
 const message = document.querySelector('.message');
 const score = document.querySelector('.score');
 const highscore = document.querySelector('.highscore');
+const changeBackground = document.querySelector('body');
 
-// guessing.addEventListener('click', e => {
-//   guessing.textContent = 'Correct answer!';
-// });
+// random number generator between 1 - 20
+const secretNumber = Math.floor(Math.random() * 20);
+randomNumber.textContent = secretNumber;
+
+check.addEventListener('click', e => {
+  let guess = Number(checkInput.value);
+
+  if (!guess) {
+    message.textContent = '⚠️ No number guessed!';
+  } else if (guess === secretNumber) {
+    message.textContent = '🙌 Correct number!';
+    randomNumber.style.backgroundColor = 'lightred';
+  } else if (guess > secretNumber) {
+    if (score.textContent > 1) {
+      message.textContent = '⬆️ Too high!';
+      score.textContent -= 1;
+    } else {
+      message.textContent = 'You lost the game...😕';
+      randomNumber.style.backgroundColor = 'lightred';
+      score.textContent = 0;
+    }
+  } else if (guess < secretNumber) {
+    if (score.textContent > 1) {
+      message.textContent = '⬇️ Too low!';
+      score.textContent -= 1;
+    } else {
+      message.textContent = 'You lost the game...😕';
+      randomNumber.style.backgroundColor = 'red';
+      score.textContent = 0;
+    }
+  }
+});

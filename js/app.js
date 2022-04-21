037fe374ee732c6ffad3c56086d5cb3496b73538
tonioreign <tonioreign@gmail.com>
2022-04-21 13:53:52 -0700

    Added Play Again function

--- js/app.js
+++ js/app.js
@@ -10,24 +10,31 @@ const highscore = document.querySelector('.highscore');
 const changeBackground = document.querySelector('body');
 
 // random number generator between 1 - 20
-const secretNumber = Math.floor(Math.random() * 20);
-randomNumber.textContent = secretNumber;
+let secretNumber = Math.floor(Math.random() * 20);
 
+// when clicked checking for correct answer
 check.addEventListener('click', e => {
   let guess = Number(checkInput.value);
 
   if (!guess) {
     message.textContent = '⚠️ No number guessed!';
   } else if (guess === secretNumber) {
+    // only reveals secret number after getting it right or wrong
+    randomNumber.textContent = secretNumber;
     message.textContent = '🙌 Correct number!';
-    randomNumber.style.backgroundColor = 'lightred';
+    randomNumber.style.backgroundColor = 'lightgreen';
+    randomNumber.style.width = '15rem';
+    highscore.textContent = score.textContent;
   } else if (guess > secretNumber) {
+    //nested if statements to end game when score is 0
     if (score.textContent > 1) {
       message.textContent = '⬆️ Too high!';
       score.textContent -= 1;
     } else {
       message.textContent = 'You lost the game...😕';
-      randomNumber.style.backgroundColor = 'lightred';
+      randomNumber.textContent = secretNumber;
+      randomNumber.style.backgroundColor = 'red';
+      randomNumber.style.width = '15rem';
       score.textContent = 0;
     }
   } else if (guess < secretNumber) {
@@ -36,8 +43,20 @@ check.addEventListener('click', e => {
       score.textContent -= 1;
     } else {
       message.textContent = 'You lost the game...😕';
+      randomNumber.textContent = secretNumber;
       randomNumber.style.backgroundColor = 'red';
+      randomNumber.style.width = '15rem';
       score.textContent = 0;
     }
   }
+});
+
+againBtn.addEventListener('click', e => {
+  score.textContent = 20;
+  message.textContent = 'Start guessing...';
+  randomNumber.style.backgroundColor = '#ffffff';
+  randomNumber.style.width = '200px';
+  randomNumber.textContent = '?';
+  secretNumber = Math.floor(Math.random() * 20);
+  checkInput.value = ' ';
 });

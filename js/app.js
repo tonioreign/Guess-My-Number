a731ea9ab6ef051a07d0b2250a471c91e3d855d5
tonioreign <tonioreign@gmail.com>
2022-04-21 11:46:48 -0700

    Grabbed elements to muniplulate

--- js/app.js
+++ js/app.js
@@ -1,1 +1,13 @@
 'use strict';
+
+const againBtn = document.querySelector('.again');
+const randomNumber = document.querySelector('.number');
+const guess = document.querySelector('.guess');
+const check = document.querySelector('.check');
+const message = document.querySelector('.message');
+const score = document.querySelector('.score');
+const highscore = document.querySelector('.highscore');
+
+// guessing.addEventListener('click', e => {
+//   guessing.textContent = 'Correct answer!';
+// });
